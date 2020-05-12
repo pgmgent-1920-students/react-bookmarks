08a46d516bf6f5000a12ff8aa5c56daf9905201a
drdynscript <drdynscript@gmail.com>
2020-05-12 14:07:46 +0200

    Added firebase functionality

--- scripts/messages_seeder.js
+++ scripts/messages_seeder.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+import faker from 'faker';
 import { admin, app, db, generateTimestamps } from './firebase';
 
 (async () => {
@@ -8,6 +10,8 @@ import { admin, app, db, generateTimestamps } from './firebase';
   let collectionRef = db.collection('messages');
   // Loop through the documents of the collection
   const result = await collectionRef.get().then(querySnapshot => {
+    const size = querySnapshot.size;
+    console.log(size);
     querySnapshot.forEach(documentSnapshot => {
       const id = documentSnapshot.id;
       messages.push({
@@ -17,5 +21,59 @@ import { admin, app, db, generateTimestamps } from './firebase';
     });
   });
   console.log(messages);
+
+  // 1. Add a document
+  /*collectionRef.add({foo: 'bar'}).then(documentReference => {
+    console.log(`Added document with name: ${documentReference.id}`);
+  });*/
+
+  // 2. Add a document
+  /*let documentRef = collectionRef.doc();
+  documentRef.create({foo: 'bar'}).then((res) => {
+    console.log(`Document created at ${res.writeTime.toDate()}`);
+  }).catch((err) => {
+    console.log(`Failed to create document: ${err}`);
+  });*/
+
+  // 3. Add a document
+  /*collectionRef.doc(uuidv4()).create({bar: 'foo'}).then((res) => {
+    console.log(`Document created at ${res.writeTime.toDate()}`);
+  }).catch((err) => {
+    console.log(`Failed to create document: ${err}`);
+  });*/
+
+  const createMessage = (content, sender) => {
+    // Add a document with faker
+    const data = {
+      content: content,
+      sender: sender,
+      createdAt: Date.now()
+    };
+
+    collectionRef.add(data).then(documentReference => {
+      console.log(`Added document with name: ${documentReference.id}`);
+    });
+  };
+
+  
+  for (let i=0; i < 200;i++) {
+    createMessage(faker.lorem.paragraph(), faker.name.findName());
+  }
+
 })();
 
+
+
+/*
+collectionRef.add({foo: 'bar'}).then(documentReference => {
+  console.log(documentReference.path);
+});
+
+collectionRef.doc().create({bar: 'foo'}).then(writeResult => {
+  console.log(writeResult);
+});
+
+collectionRef.doc('pol').set({bar: 'foo'}, { merge: true}).then(writeResult => {
+  console.log(writeResult);
+});
+*/
