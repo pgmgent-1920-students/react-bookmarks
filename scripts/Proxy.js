a827a9d85ccfebf5501d857ce3118f759c3b3001
drdynscript <drdynscript@gmail.com>
2020-05-12 14:33:08 +0200

    Added Proxy for SEO crawler

--- scripts/Proxy.js
+++ scripts/Proxy.js
@@ -1,10 +1,10 @@
-class Proxy {
-  constructor() {
-    this.PROXY_SEO_URL = 'https://nodejs-nmd-crawler.herokuapp.com/proxy/seo?url=';
-  }
+import fetch from 'node-fetch';
 
+const PROXY_SEO_URL = 'https://nodejs-nmd-crawler.herokuapp.com/proxy/seo?url=';
+
+class Proxy { 
   static async getSEOFromUrl(url) {
-    const response = await fetch(`${this.PROXY_SEO_URL}${url}`);
+    const response = await fetch(`${PROXY_SEO_URL}${url}`);
     const jsonData = await response.json();
     return jsonData;
   }; 
