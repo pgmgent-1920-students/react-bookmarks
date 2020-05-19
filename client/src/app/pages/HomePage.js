a84785cf6bbeb077de06fce53ca76c92f37f8eab
drdynscript <drdynscript@gmail.com>
2020-05-19 07:05:52 +0200

    More bootstrap styling

--- client/src/app/pages/HomePage.js
+++ client/src/app/pages/HomePage.js
@@ -1,16 +1,18 @@
 import React, { } from 'react';
 
+import './HomePage.scss';
+
 const HomePage = ({children}) => {
   return (
     <div className="page page--home">
-      <section className="pt-4 pt-md-11">
+      <section className="pt-4 pt-md-11 featured">
         <div className="container">
           <div className="row align-items-center">
             <div className="col-12 col-md-5 col-lg-6 order-md-2">              
             </div>
             <div className="col-12 col-md-7 col-lg-6 order-md-1 aos-init aos-animate">
               <h1 className="display-3 text-center text-md-left">
-                Welcome to <span className="text-primary">Firebase React Boilerplate</span>. <br />
+                Welcome to <span className="font-weight-bold">Firebase React Boilerplate</span>. <br />
                 Firebase everything.
               </h1>              
             </div>
